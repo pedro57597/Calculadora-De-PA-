305afb5a50debdabc87d594b2e7d792a08cc88d4
Pedro H <79934413+pedro57597@users.noreply.github.com>
2021-11-30 08:45:32 -0300

    atualização

---
+++ mostrar MAIS-MENOS/js/meuscript.js
@@ -0,0 +1,18 @@
+var conteudo = document.getElementById('conteudo');
+var botao = document.getElementById('mostre_mais');
+
+//criar o evento do click do botão utilizando uma função anônima
+botao.onclick = function(){
+
+    if(conteudo.className == "open"){
+        //encurtar a caixa
+        conteudo.className = ""; //muda a classe da div conteudo para ""
+        botao.innerHTML = "Mostrar Mais"; //muda o texto do botão para Mostrar Mais
+    }
+    else{
+        //expandir a caixa
+        conteudo.className = "open"; //muda a classe da div conteudo para open 
+        botao.innerHTML = "Mostrar Menos"; //muda o texto do botão para Mostrar Menos
+    }
+    
+};
